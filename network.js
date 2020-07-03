0a6fe3fc1a737477b93f86b64ed46a4a191d72b3
hagyeong <gkrud4513@naver.com>
2020-07-03 11:52:22 +0900

    network

---
+++ network.js
@@ -0,0 +1,32 @@
+let network = [];
+
+function solution(n, computers) {
+  let visit = []
+  visit.length = n;
+  visit.fill(false,0,n);
+  var answer = 0;
+
+
+  for(let i=0;i<n;i++){
+    if(visit[i]==false){
+      check(computers,visit,i);
+      answer++;
+    }
+  }
+  
+  return answer;
+}
+
+function check(c,v,index) {
+  if(v[index]==true) return;
+  else{
+    v[index]=true;
+    for(let i=0;i<c.length;i++){
+      if(c[index][i]==1){
+        check(c,v,i)
+      }
+    }
+  }
+}
+
+console.log(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]));
